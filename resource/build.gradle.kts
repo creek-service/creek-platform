947df2c1ca061f5e311e5d8e312c08e769ee61cc
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-11-05 23:15:54 +0000

    Simplify Creek Dependencies (#42)

--- resource/build.gradle.kts
+++ resource/build.gradle.kts
@@ -2,11 +2,10 @@ plugins {
     `java-library`
 }
 
-val creekBaseVersion : String by extra
-val creekObsVersion : String by extra
+val creekVersion : String by extra
 
 dependencies {
     api(project(":metadata"))
-    implementation("org.creekservice:creek-base-type:$creekBaseVersion")
-    implementation("org.creekservice:creek-observability-logging:$creekObsVersion")
+    implementation("org.creekservice:creek-base-type:$creekVersion")
+    implementation("org.creekservice:creek-observability-logging:$creekVersion")
 }
