4f5af45d407f46ce2f73980dfbf0c81133593ec2
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2023-01-27 14:38:30 +0000

    Update to latest conventions (#67)
    * Update to latest conventions

--- metadata/src/main/java/module-info.java
+++ metadata/src/main/java/module-info.java
@@ -1,6 +1,14 @@
 /** Dependency free module containing base interfaces for defining components */
+import org.creekservice.api.platform.metadata.ComponentDescriptor;
+
+/**
+ * Platform Metadata Module.
+ *
+ * <p>A dependency-free module that defines the interfaces used to describe platform components and
+ * resources.
+ */
 module creek.platform.metadata {
     exports org.creekservice.api.platform.metadata;
 
-    uses org.creekservice.api.platform.metadata.ComponentDescriptor;
+    uses ComponentDescriptor;
 }
